a0b150fec0b56da3dce0ac31f1e4f7c5b78714fc
Manuel1977 <Manuel_fe1977@hotmail.com>
2021-04-03 11:18:10 +0200

    Version 1.0.0 en esta version solo tenemos HTML5 y CSS3

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+function aceptalopd(opcion)
+{
+    if (opcion)/*acepta la lopd habilitamos boton submit*/
+    {
+        document.getElementById('btnSubmit').disabled=false;
+
+    }
+    else/*no acepta la lopd deshabilitamos boton submit*/
+    {
+        document.getElementById('btnSubmit').disabled=true;
+    }
+}
+
+function resetbtnSubmmit()
+{
+    document.getElementById('btnSubmit').disabled=true;
+}
+
+function validar(formularioPresupuesto)
+{
+    if (formularioPresupuesto.nombre.value.length==0){
+        alert("Indique un nombre");
+        return false;
+    }
+    if (formularioPresupuesto.email.value.length==0)
+    {
+        alert("Indique un email");
+        return false;
+    }
+    listacaracteres=/^[_a-z0-9-]+(.[a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$/
+    if (!listacaracteres.test(document.getElementById('email').value))
+    {
+        alert("Debe indicar un email valido");
+        return false;
+    }
+    if (formularioPresupuesto.movil.value.length==0) {
+        alert("Indique un movil de contacto");
+        return false;
+    }
+    listacaracteres=/^[0-9]{9}$/
+    if (!listacaracteres.test(document.getElementById('movil').value))
+    {
+        alert("Debe indicar un movil valido");
+        return false;
+    }
+    alert("Formulario correcto");
+    formularioPresupuesto.submit();
+
+}
