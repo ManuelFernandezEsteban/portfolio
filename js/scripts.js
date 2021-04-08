83c6791fe685b2b9254287633fc9498a6ce54fbd
Manuel1977 <Manuel_fe1977@hotmail.com>
2021-04-08 07:00:05 +0200

    navegacion concluida, surge problema con aside

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,88 @@
 var fuera=false;
 var saludo;
-$(document).ready(function(){
+var objetoHttp=null;
+
+
+function navega(enlace){
+    console.log(enlace);
+    $("#main").load(enlace);     
+}
+
+function escribirNoticia(){
+    if (objetoHttp.readyState==4){
+        var documento = objetoHttp.responseXML;
+        var texto = documento.documentElement;
+        var cadenaAEscribir = "";
+        var i = 0;
+        while (i<=6){
+            cadenaAEscribir = cadenaAEscribir + "<div class=\"contenido-caja-noticia\"><h3>"+texto.getElementsByTagName("item")[i].childNodes[4].firstChild.nodeValue+"</h3>";
+            cadenaAEscribir = cadenaAEscribir + "<div>"+texto.getElementsByTagName("item")[i].childNodes[6].firstChild.nodeValue+"</div>";
+            cadenaAEscribir = cadenaAEscribir + "<a href=\""+texto.getElementsByTagName("item")[i].childNodes[2].firstChild.nodeValue+"\" target=\"_blank\">"+texto.getElementsByTagName("item")[i].childNodes[2].firstChild.nodeValue+"</a></div>";
+            i++;
+        }
+        //document.getElementById("cajaNoticias").innerHTML=cadenaAEscribir;
+        //console.log(cadenaAEscribir);
+        $("#cajaNoticias").html(cadenaAEscribir);
+        
+    }
+}
+function cargarInicio(){
+
+    navega('cuerpoInicio.html');
+    if(window.XMLHttpRequest){
+        objetoHttp=new XMLHttpRequest();        
+    }else if (window.ActiveXObject){
+        objetoHttp= new ActiveXObject("Microsoft.XMLHTTP")
+    }
+    objetoHttp.open("GET","xml/rss.xml",true);
+    objetoHttp.onreadystatechange=escribirNoticia;
+    objetoHttp.send(null);
     saludo = setTimeout(mensaje,5000); 
     $('.caja-disparador p').css("align-self","center"); 
     $('.noticias').css('flex-grow','0');  //cualquier otro tamaño          
-    document.getElementById("cajaNoticias").style.display="none";
+    $('#cajaNoticias').css('display','none');
     if (screen.width<768){ //tamaño movil
-        document.getElementById("contenedor-secundario").style.display="block";
+        //document.getElementById("contenedor-secundario").style.display="block";
+        $('#contenedor-secundario').css('display','block');
     }
+}
+function iniciar(){
+    console.log("hola");
+    cargarInicio();
+}
+
+function mensaje(){
+    alert("Bienvenidos a mi portfolio");
+    clearTimeout(saludo);
+}
+function navega(enlace){
+    console.log(enlace);
+    $("#main").load(enlace);  
     
+}
+
+
+
+
+
+
+
+
+$(document).ready(function(){    
+
+    iniciar();
+
     $('.caja-disparador p').click(function(){
         //alert("hola");
         if (fuera){            
             fuera=false;   
             $('.caja-disparador p').css("align-self","center"); 
-            $('.noticias').css('flex-grow','0');  //cualquier otro tamaño          
-            document.getElementById("cajaNoticias").style.display="none";
+            $('.noticias').css('flex-grow','0');  //cualquier otro tamaño    
+            $('#cajaNoticias').css('display','none');      
+            //document.getElementById("cajaNoticias").style.display="none";
             if (screen.width<768){ //tamaño movil
-                document.getElementById("contenedor-secundario").style.display="block";
+                //document.getElementById("contenedor-secundario").style.display="block";
+                $('#contenedor-secundario').css('display','block');
             }
         }else{ 
             fuera=true; 
@@ -25,7 +90,8 @@ $(document).ready(function(){
                 $('.noticias').css('flex-grow','4');
             } 
             if (screen.width<768){//tamaño movil
-                document.getElementById("contenedor-secundario").style.display="none";
+                //document.getElementById("contenedor-secundario").style.display="none";
+                $('#contenedor-secundario').css('display','none');
             }
            else if (screen.width>768){  //tamaño desktop o superior                                 
                $('.noticias').css('flex-grow','3');
@@ -46,28 +112,28 @@ $(document).ready(function(){
     })    
     
 })
-function navega(enlace){
-    console.log(enlace);
-    $("#main").load(enlace,'#main');
-     
-}
 
-
-$('.menu-enlace').on('click',function(event){
+$('.caja-logo a').on('click',function(event){
+    console.log("hola");
     event.preventDefault();
     let destino=($(this).attr('href'));
+    $('.seleccionado').removeClass('seleccionado');
+    $('a[href$="cuerpoInicio.html"').addClass('seleccionado');
+    console.log(destino);
+    navega(destino);
+});
+
+$('.menu-enlace').on('click',function(event){
+    console.log("hola");
+    event.preventDefault();
+    let destino=($(this).attr('href'));
+    $('.seleccionado').removeClass('seleccionado'); 
+    $(this).addClass('seleccionado');  
     console.log(destino);
     navega(destino);
 });
 
 
-
-
-
-function mensaje(){
-    alert("Bienvenidos a mi portfolio");
-    clearTimeout(saludo);
-}
 $(window).resize(function(){//cerramos el panel de noticias
     if (fuera){            
         fuera=false;   
@@ -80,36 +146,7 @@ $(window).resize(function(){//cerramos el panel de noticias
     }
 });
 
-var objetoHttp=null;
-function escribirNoticia(){
-    if (objetoHttp.readyState==4){
-        var documento = objetoHttp.responseXML;
-        var texto = documento.documentElement;
-        var cadenaAEscribir = "";
-        var i = 0;
-        while (i<=6){
-            cadenaAEscribir = cadenaAEscribir + "<div class=\"contenido-caja-noticia\"><h3>"+texto.getElementsByTagName("item")[i].childNodes[4].firstChild.nodeValue+"</h3>";
-            cadenaAEscribir = cadenaAEscribir + "<div>"+texto.getElementsByTagName("item")[i].childNodes[6].firstChild.nodeValue+"</div>";
-            cadenaAEscribir = cadenaAEscribir + "<a href=\""+texto.getElementsByTagName("item")[i].childNodes[2].firstChild.nodeValue+"\" target=\"_blank\">"+texto.getElementsByTagName("item")[i].childNodes[2].firstChild.nodeValue+"</a></div>";
-            i++;
-        }
-        document.getElementById("cajaNoticias").innerHTML=cadenaAEscribir;
-        
-    }
-}
 
-
-function cargarNoticia(){
-    
-    if(window.XMLHttpRequest){
-        objetoHttp=new XMLHttpRequest();        
-    }else if (window.ActiveXObject){
-        objetoHttp= new ActiveXObject("Microsoft.XMLHTTP")
-    }
-    objetoHttp.open("GET","xml/rss.xml",true);
-    objetoHttp.onreadystatechange=escribirNoticia;
-    objetoHttp.send(null);
-}
 
 
 
