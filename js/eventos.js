77ad30175573e1f9375e2f64cb9e2d8f6e91dadb
Manuel1977 <Manuel_fe1977@hotmail.com>
2021-04-15 07:09:12 +0200

    atascado en la galeria

--- js/eventos.js
+++ js/eventos.js
@@ -2,25 +2,33 @@
 $(document).ready(function(){ 
      
     $('.ficha').on('click',function(){            
-        let fichas = $('.ficha');
         
-        fichas.each(function () {
-            $(this).addClass('Oculta');
+        $('.ficha').each(function(){
+            $(this).css('display','none');
         })
-        $(this).css('cursor', 'auto');
-        $(this).removeClass('Oculta');
-        $(this).css('width', '100%');
-        $(this).css('height', '100%');
+        $(this).css('display','block');
         $(this).children('.in').css('display','block');
+        $(this).css('cursor', 'auto');
+        $(this).removeClass('Oculta');//mostramos la imagen seleccionada
+        $(this).css('width', '100%');//aumentamos los tamaños
+        $(this).css('height', '100%');        
         $(this).children('.img').css('width', '100%');
         $(this).children('.img').css('height', '100%');
         $(this).children('.img').children('img').css('width', '90%');
         $(this).children('.img').children('img').css('height', '90%');
         $(this).children('.texto-imagen').css('display', 'flex');   
-        $('.galeria').css('grid-template-columns', '1fr');
+        $('.galeria').css('grid-template-columns', '1fr');//ponemos el grid con una sola celda
     });
 
-    $('.in').on('click',function(){  
+    $('.in').on('click',function(){          
+
+        $('.ficha').each(function(){
+            $(this).css('display','block');            
+            console.log(this);
+        })
+        
+        
+        
 
         $(this).css('display','none');
         
