558f634765047c0864d21b85f31b9ef11d3881bb
Manuel1977 <Manuel_fe1977@hotmail.com>
2021-04-21 21:34:58 +0200

    integrando la  galeria en el portfolio

--- js/eventos-galeria.js
+++ js/eventos-galeria.js
@@ -1,29 +1,67 @@
-const imagenes = document.querySelectorAll('.img-galeria');
-//const imagenesLight = document.querySelector('.agregar-imagen');
-const textoLight = document.querySelector('.agregar-texto');
-const contenedorLight = document.querySelector('.imagen-light');
-const imagenGrande = document.querySelector('.imagen');
 
-imagenes.forEach(imagen =>{
-    imagen.addEventListener('click', ()=>{
-        aparecerImagen(imagen.getAttribute('src'))
-    
+$(document).ready(function(){
+    const imagenes = document.querySelectorAll('.img-galeria');
+    //const imagenesLight = document.querySelector('.agregar-imagen');
+    const textoLight = document.querySelector('.agregar-texto');
+    var contenedorLight = document.querySelector('.imagen-light');
+    const imagenGrande = document.querySelector('.imagenGaleria');
+    const descripcion = document.querySelector('.agregar-texto p');
+    const encabezado = document.querySelector('.agregar-texto h1');
+    var fotosPeluqueria;
+
+
+
+    imagenes.forEach(imagen => {
+        imagen.addEventListener('click', () => {
+            aparecerImagen(imagen)
+
+        })
     })
-})
 
-contenedorLight.addEventListener('click', (e) =>{
-    if(e.target !== imagenGrande){
-        contenedorLight.classList.remove('show');
-       // imagenesLight.classList.remove('showImage');
-        textoLight.classList.remove('showImage');
-        imagenGrande.classList.remove('showImage')
+    contenedorLight.addEventListener('click', (e) => {
+        if (e.target !== imagenGrande) {
+            contenedorLight.classList.remove('show');
+            // imagenesLight.classList.remove('showImage');
+            textoLight.classList.remove('showImage');
+            imagenGrande.classList.remove('showImage')
+        }
+    })
+
+    const aparecerImagen = (imagen) => {
+        imagenGrande.src = imagen.getAttribute('src');
+        imagenGrande.classList.add('showImage')
+        contenedorLight.classList.add('show');
+        // imagenesLight.classList.add('showImage');
+        textoLight.classList.add('showImage');
+        descripcion.innerHTML = fotosPeluqueria[imagen.getAttribute('numero')].descripcion;
+        encabezado.innerHTML = fotosPeluqueria[imagen.getAttribute('numero')].titulo;
+    }
+
+    function cargarFotos(objetoFotos) {
+        var limite = 0;
+        if (objetoFotos.length > imagenes.length) {
+            limite = imagenes.length - 1;
+        }
+        else {
+            limite = objetoFotos.length - 1;
+        }
+        console.log(limite);
+        for (var i = 0; i <= limite; i++) {
+            imagenes[i].setAttribute('src', objetoFotos[i].src);
+            imagenes[i].setAttribute('numero', i);
+        }
+
+    }
+
+    function traerFotos() {
+        const requestURL = "json/peluqueria.json";
+        const request = new XMLHttpRequest();
+        request.open('GET', requestURL);
+        request.responseType = 'json';
+        request.send();
+        request.onload = function () {
+            fotosPeluqueria = request.response;
+            cargarFotos(fotosPeluqueria);
+        }
     }
 })
-
-const aparecerImagen = (imagen) =>{
-    imagenGrande.src = imagen;
-    imagenGrande.classList.add('showImage')
-    contenedorLight.classList.add('show');
-   // imagenesLight.classList.add('showImage');
-    textoLight.classList.add('showImage');
-}
